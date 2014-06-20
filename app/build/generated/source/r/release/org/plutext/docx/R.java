e3230af004dbc6d72303610a1ca75f270ae0be93
orient33 <orient33@163.com>
2014-06-20 18:58:41 +0800

    调整资源etc

--- app/build/generated/source/r/release/org/plutext/docx/R.java
+++ app/build/generated/source/r/release/org/plutext/docx/R.java
@@ -14,15 +14,12 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int webpage=0x7f060000;
+        public static final int webpage=0x7f050000;
     }
     public static final class layout {
         public static final int main=0x7f030000;
     }
-    public static final class raw {
-        public static final int sample=0x7f040000;
-    }
     public static final class string {
-        public static final int app_name=0x7f050000;
+        public static final int app_name=0x7f040000;
     }
 }
--- app/src/main/java/com/singuloid/docx2html/DocxShow.java
+++ app/src/main/java/com/singuloid/docx2html/DocxShow.java
@@ -1,8 +1,5 @@
 package com.singuloid.docx2html;
 
-
-import java.io.InputStream;
-
 import android.app.Activity;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -22,25 +19,22 @@ public class DocxShow extends Activity {
         setContentView(R.layout.main);
 
         webview = (WebView) this.findViewById(R.id.webpage);
-        webview.loadData("Loading docx file...,compile on AndroidStudio  ", "text/html", "UTF-8");
-        String path = getIntent().getStringExtra("dst");
-
-        final InputStream is = this.getResources().openRawResource(R.raw.sample);
+        webview.loadData("Loading docx file... ", "text/html", "UTF-8");
+        final String path = getIntent().getStringExtra("dst");
         if (!TextUtils.isEmpty(path)) {
             setTitle(path);
         } else {
-            setTitle("sample.docx");
             Log.e(TAG, "path is null. use R.raw.sample  Resource!!");
-            path = "/sdcard/docx_src.docx";
+            finish();
         }
-        Log.d("FE", TAG + "] + onCreate() path=" + path);
+        Log.d(TAG," onCreate()  file path = " + path);
         AsyncTask<String, Object, Object> at = new AsyncTask<String, Object, Object>() {
             String baseURL = "";
 
             @Override
             protected Object doInBackground(String... arg) {
                 final String html = new DocxToHtml().convert(DocxShow.this, DocxShow.this, arg[0]);
-                Log.d(TAG, "[html== ] " + html);
+                Log.d(TAG, "[html= ] " + html);
                 return html;
             }
 
@@ -49,8 +43,8 @@ public class DocxShow extends Activity {
                 webview.loadDataWithBaseURL(baseURL, (String) html, "text/html", null, null);
             }
         };
-
-        at.execute(path);
+        if(!TextUtils.isEmpty(path))
+            at.execute(path);
     }
 
 }
